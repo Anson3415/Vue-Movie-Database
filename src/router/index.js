0bd5b94dc6d90cc37bc4cf68f346fe48c66933bf
Anson Su <anson3415@gmail.com>
2022-12-02 15:13:52 -0500

    added some files to setup router

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Account from "../views/Account.vue";
+
+const routes = [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/account',
+    component: Account
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
